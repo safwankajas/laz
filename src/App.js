7bf0772cf2b5828a16c1e6f2d22d9fe8e0a58318
safwankajas <84768183+safwankajas@users.noreply.github.com>
2023-04-29 00:38:01 +0530

    demo

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+
+export default function App() {
+  function Submit(e) {
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbz71iwfkuATHdwOu6SeYBGwDdCxpiFzIHPnHZjXRSJ0RxqTA35fn7oWWbp_UwJmK9ip/exec",
+      {
+        method: "POST",
+        body: formDatab
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return (
+    <div className="App">
+      <h1>Contact Me form</h1>
+      <h2>
+        This demonstrates how to send data from a website form to Google sheet
+        in React or Vanilla jS
+      </h2>
+      <div>
+        <form className="form" onSubmit={(e) => Submit(e)}>
+          <input placeholder="Your Name" name="Name" type="text" />
+          <input placeholder="Your Email" name="Email" type="text" />
+          <input placeholder="Your Message" name="Phone" type="text" />
+          <input name="Name" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
